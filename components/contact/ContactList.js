3e720c71283a8e3bd8befa03cd06a177f299d566
afiatamanna06 <afiatamanna06@gmail.com>
2022-06-04 23:50:07 +0600

    Add footer

---
+++ components/contact/ContactList.js
@@ -0,0 +1,26 @@
+import { FiMail, FiClock, FiMapPin, FiPhone } from "react-icons/fi"
+
+const contactList = [
+    {
+        Icon: FiMail,
+        title: "Our Email",
+        data: "abc@gmail.com"
+    },
+    {
+        Icon: FiClock,
+        title: "Opening Hours",
+        data: "07:00am To 09:00pm"
+    },
+    {
+        Icon: FiMapPin,
+        title: "Shop Location",
+        data: "Banani, Dhaka"
+    },
+    {
+        Icon: FiPhone,
+        title: "Our Number",
+        data: "+880-1778-8990"
+    },
+]
+
+export default contactList
--- components/contact/ContactSection.js
+++ components/contact/ContactSection.js
@@ -1,9 +1,10 @@
-import { Box, Flex, Grid, Image, Center, Input, Button } from '@chakra-ui/react'
+import { Box, Flex, Grid, Image, Center, Input, Button, Heading, Divider } from '@chakra-ui/react'
 import Header from '../common/Header'
+import contactList from './ContactList'
 
 function ContactSection() {
   return (
-    <Box display="flex" flexDir="column" alignItems="center" py="16" px="4" id="Contact">
+    <Box display="flex" flexDir="column" alignItems="center" py="16" px="4" id="Contact" borderColor="rgba(190, 156, 121, 1)">
         <Header title="Contact Us" color={["rgba(255, 255, 255, 1)"]} />
         <Flex alignItems="center" direction={["column", "column", "column", "row", "row"]}>
             <Image flex="1" src="/contact-img.svg" w="700px" h="700px" alt="" />
@@ -15,6 +16,17 @@ function ContactSection() {
                 <Button px={8} py={4} color="white" borderRadius="none" fontWeight="normal" bg="rgba(190, 156, 121, 1)" _hover={{ bg: "rgb(238, 230, 221)", borderColor: "rgb(238, 230, 221)", color: "rgb(119, 91, 60)" }} _active={{ bg: "rgb(238, 230, 221)", borderColor: "rgb(238, 230, 221)", color: "rgb(119, 91, 60)" }} _focus={{ bg: "rgb(238, 230, 221)", borderColor: "rgb(238, 230, 221)", color: "rgb(119, 91, 60)", outline: "none" }}>Send Message</Button>
             </Flex>
         </Flex>
+        <Grid templateColumns={{ base: "repeat(2, 1fr)", sm: "repeat(2, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(4, 1fr)"}} gap={24} py={16}>
+            {contactList.map(({title,data,Icon}) => (       
+                <Flex key={title} alignItems="center" flexDir="column" textAlign="center">
+                    <Box bg="rgba(190, 156, 121, 1)" p="5" rounded="full" mb="6" maxW="min-content"><Icon size={30} color="white" /></Box>
+                    <Heading color="white" fontSize="2xl" mb="4">{title}</Heading>
+                    <Box color="gray.400">{data}</Box>
+                </Flex>
+            ))}
+        </Grid>
+        <Divider maxW="70rem" />
+        <Flex color="white" mt="4" fontSize="xl" fontWeight="hairline" direction={["column", "column", "column", "row", "row"]}>© Copyright @ 2022 By<Box color="rgba(190, 156, 121, 1)" mx="2">Afia Anjum Tamanna</Box> | All Rights Reserved!</Flex>
     </Box>
   )
 }
