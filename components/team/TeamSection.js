4ce05e49c6503a8f566b4e4058313b5b7f0951f1
afiatamanna06 <afiatamanna06@gmail.com>
2022-06-05 13:23:51 +0600

    Update background color

--- components/team/TeamSection.js
+++ components/team/TeamSection.js
@@ -8,7 +8,7 @@ function TeamSection() {
         <Header title="Our Team" color={["rgba(255, 255, 255, 1)"]} />
         <Grid templateColumns={["repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(3, 1fr)", "repeat(3, 1fr)"]} alignItems="center" maxW="80rem" mt="2" columnGap={["2"]} rowGap={["2"]}>
           {teamData.map(({image, title}) => (
-            <Flex key={title} direction="column" alignItems="center" maxW={["15rem", "15rem", "15rem", "20rem", "20rem"]} maxH={["15rem", "15rem", "15rem", "20rem", "20rem"]} p="5" _hover={{ bg: "rgba(255, 255, 255, .075)", cursor: "pointer" }}>
+            <Flex key={title} direction="column" alignItems="center" maxW={["15rem", "15rem", "15rem", "20rem", "20rem"]} maxH={["15rem", "15rem", "15rem", "20rem", "20rem"]} p="5" _hover={{ bg: "rgba(0, 0, 0, .1)", cursor: "pointer" }}>
               <Image src={image} mb="2" w="250px" h="250px" objectFit="cover" alt="" />
               <Heading textAlign="center" fontSize="xl" color={["white"]}>{title}</Heading>
             </Flex>
