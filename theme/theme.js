1da324b4031d5225d513ffde6ca9ce5659bc16cc
afiatamanna06 <afiatamanna06@gmail.com>
2022-06-05 11:47:54 +0600

    Update theme

--- theme/theme.js
+++ theme/theme.js
@@ -1,15 +1,16 @@
-// theme.js
-
-// 1. import `extendTheme` function
 import { extendTheme } from '@chakra-ui/react'
 
-// 2. Add your color mode config
 const config = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
 }
 
-// 3. extend the theme
-const theme = extendTheme({ config })
+const fonts = {
+  heading: "Jost, sans-serif",
+  body: "Poppins, sans-serif",
+  mono: "JetBrains Mono, monospace",
+};
+
+const theme = extendTheme({ config, fonts })
 
 export default theme
